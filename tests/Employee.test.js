217e57d2cc935f42ea0a3ec28eb871055fca514a
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-12-17 14:53:23 -0500

    added functions to classes

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -3,26 +3,26 @@ const Employee = require('../lib/Employee.js');
 describe('Get', () => {
     it('Get the name of the employee entered by the user', () => {
         const employee = new Employee('Daniel', 1, 'example@gmail.com')
-        const result = getName(new Employee('Daniel', 1, 'example@gmail.com'))
+        const result = Employee.getName(new Employee('Daniel', 1, 'example@gmail.com'))
       expect(result).toEqual(employee.name);
     });
 
     it('Get the id of the employee entered by the user', () => {
         const employee = new Employee('Daniel', 1, 'example@gmail.com')
-        const result = getid(new Employee('Daniel', 1, 'example@gmail.com'))
+        const result = Employee.getId(new Employee('Daniel', 1, 'example@gmail.com'))
       expect(result).toEqual(employee.id);
     });
 
     it('Get the email of the employee entered by the user', () => {
         const employee = new Employee('Daniel', 1, 'example@gmail.com')
-        const result = getEmail(new Employee('Daniel', 1, 'example@gmail.com'))
+        const result = Employee.getEmail(new Employee('Daniel', 1, 'example@gmail.com'))
       expect(result).toEqual(employee.email);
     });
 
     it('Get the role of the employee entered by the user', () => {
         const employee = new Employee('Daniel', 1, 'example@gmail.com')
         employee.role = "Employee";
-        const result = getRole();
+        const result = Employee.getRole();
       expect(result).toEqual(employee.role);
     });
 })
