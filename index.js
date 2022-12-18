5c225f1d6180d0558af9f3dc41567e9cf15b4195
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-12-17 21:29:34 -0500

    fixed prompts

--- index.js
+++ index.js
@@ -80,7 +80,6 @@ function internPrompts() {
             console.log(answers);
             memberPrompt();
         });
-
 }
 
 console.log('Welcome to your team profile generator!!')
@@ -107,75 +106,15 @@ inquirer
             name: 'officeNumber',
             message: "What is the team manager's office number?",
         },
-        {
-            type: 'list',
-            message: 'Which type of team member would you like to add?',
-            name: 'teamMember',
-            choices: ['Engineer', 'Intern', 'I do not want to add anymore team members'],
-        },
+
     ])
     .then((answers) => {
-        const managerInfo = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber)
+        let managerInfo = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber)
         // const memberAnswers = []
         // memberAnswers.push(answers)  
         console.log(managerInfo)
         memberPrompt();
 
-        // if (answers.teamMember === 'Engineer') {
-        //     engineerPrompts();
-        // inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'engineerName',
-        //             message: "What is the team engineer's name?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'engineerId',
-        //             message: "What is the team engineer's id?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'engineerEmail',
-        //             message: "What is the team engineer's email?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'github',
-        //             message: "What is the team engineer's office number?",
-        //         }
-        //     ])
-        //     .then(memberAnswers.push(answers))
-        // console.log(memberAnswers)
-
-        // } else if (answers.teamMember === 'Intern') {
-        //     internPrompts();
-        // inquirer
-        //     .prompt([
-        //         {
-        //             type: 'input',
-        //             name: 'internName',
-        //             message: "What is the team intern's name?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'internId',
-        //             message: "What is the team intern's id?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'internEmail',
-        //             message: "What is the team intern's email?",
-        //         },
-        //         {
-        //             type: 'input',
-        //             name: 'school',
-        //             message: "What is the team intern's school?",
-        //         }
-        //     ])
-        //     .then(memberAnswers.push(answers))
-        // console.log(memberAnswers)
         // } else {
         //     fs.writeFile('./dist/team.html', memberAnswers, (err) =>
         //         err ? console.log(err) : console.log('We have successfully created profiles for your team members')
