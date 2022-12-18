0506468b0467a7d065be477ba4193601938834f0
vargasdm <112888008+vargasdm@users.noreply.github.com>
2022-12-17 20:32:02 -0500

    made functions for the sets of prompts (doesnt work)

--- index.js
+++ index.js
@@ -2,6 +2,87 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const Manager = require('./lib/Manager.js');
 
+function memberPrompt() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Which type of team member would you like to add?',
+                name: 'teamMember',
+                choices: ['Engineer', 'Intern', 'I do not want to add anymore team members'],
+            }
+        ])
+        .then((answers) => {
+            console.log(answers);
+            if (answers.teamMember === 'Engineer') {
+                engineerPrompts();
+            } else if (answers.teamMember === 'Intern') {
+                internPrompts();
+            } return;
+        })
+}
+
+function engineerPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'engineerName',
+                message: "What is the team engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'engineerId',
+                message: "What is the team engineer's id?",
+            },
+            {
+                type: 'input',
+                name: 'engineerEmail',
+                message: "What is the team engineer's email?",
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: "What is the team engineer's office number?",
+            },
+        ])
+        .then((answers) => {
+            console.log(answers);
+            memberPrompt();
+        });
+}
+
+function internPrompts() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'internName',
+                message: "What is the team intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'internId',
+                message: "What is the team intern's id?",
+            },
+            {
+                type: 'input',
+                name: 'internEmail',
+                message: "What is the team intern's email?",
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the team intern's school?",
+            },
+        ])
+        .then((answers) => {
+            console.log(answers);
+            memberPrompt();
+        });
+
+}
+
 console.log('Welcome to your team profile generator!!')
 
 inquirer
@@ -36,68 +117,69 @@ inquirer
     .then((answers) => {
         const managerInfo = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.officeNumber)
         // const memberAnswers = []
-        // memberAnswers.push(answers)
+        // memberAnswers.push(answers)  
         console.log(managerInfo)
+        memberPrompt();
 
-        if (answers.teamMember === 'Engineer') {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'engineerName',
-                        message: "What is the team engineer's name?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'engineerId',
-                        message: "What is the team engineer's id?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'engineerEmail',
-                        message: "What is the team engineer's email?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'github',
-                        message: "What is the team engineer's office number?",
-                    }
-                ])
-                .then(callback(teamMember))
-                memberAnswers.push(answers)
-                console.log(memberAnswers)
+        // if (answers.teamMember === 'Engineer') {
+        //     engineerPrompts();
+        // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'engineerName',
+        //             message: "What is the team engineer's name?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'engineerId',
+        //             message: "What is the team engineer's id?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'engineerEmail',
+        //             message: "What is the team engineer's email?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'github',
+        //             message: "What is the team engineer's office number?",
+        //         }
+        //     ])
+        //     .then(memberAnswers.push(answers))
+        // console.log(memberAnswers)
 
-        } else if (answers.teamMember === 'Intern') {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        name: 'internName',
-                        message: "What is the team intern's name?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'internId',
-                        message: "What is the team intern's id?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'internEmail',
-                        message: "What is the team intern's email?",
-                    },
-                    {
-                        type: 'input',
-                        name: 'school',
-                        message: "What is the team intern's school?",
-                    }
-                ])
-                .then(callback(teamMember))
-                memberAnswers.push(answers)
-                console.log(memberAnswers)
-        } else {
-            fs.writeFile('./dist/team.html', memberAnswers, (err) =>
-            err ? console.log(err) : console.log('We have successfully created profiles for your team members')
-        )
-        }
-        
+        // } else if (answers.teamMember === 'Intern') {
+        //     internPrompts();
+        // inquirer
+        //     .prompt([
+        //         {
+        //             type: 'input',
+        //             name: 'internName',
+        //             message: "What is the team intern's name?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'internId',
+        //             message: "What is the team intern's id?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'internEmail',
+        //             message: "What is the team intern's email?",
+        //         },
+        //         {
+        //             type: 'input',
+        //             name: 'school',
+        //             message: "What is the team intern's school?",
+        //         }
+        //     ])
+        //     .then(memberAnswers.push(answers))
+        // console.log(memberAnswers)
+        // } else {
+        //     fs.writeFile('./dist/team.html', memberAnswers, (err) =>
+        //         err ? console.log(err) : console.log('We have successfully created profiles for your team members')
+        //     )
+        // }
+
     })
